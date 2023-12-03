9a26ae8b93ac20bdc96dd9fd34a166ca0b558c59
Adyanth123 <88449349+Adyanth123@users.noreply.github.com>
2023-12-02 18:28:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,14 @@
 import pygame
 import os
+from pygame.locals import *
 pygame.init()
 WIDTH = 300
 HEIGHT = 800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 player_image = pygame.image.load(os.path.join("Images","player.png"))
 player_image = pygame.transform.scale(player_image,(30,30))
-
 clock = pygame.time.Clock()
-class Player:
-    def __init__(self,x,y,image):
-        self.x = x
-        self.y = y
-        self.image = image
-    def draw(self):
-        WIN.blit(self.image,(self.x,self.y))
 
-    #Method to move object (special input of speedx and speedy)
-    def move(self,speedx,speedy):
-        global speedx
-        global speedy
-        self.x += speedx
-        self.y += speedy
         
 class MainRun:
     def __init__(self,WIDTH,HEIGHT):
@@ -31,14 +18,14 @@ class MainRun:
 
     def Main(self):
         #Put all variables up here
-        player = Player(60,60,player_image)
-        player.draw()
-        player.move(speedx,speedy)
+        x = 100
+        y = 100
+        vel = 5
         BLUE = 175, 238, 238
         stopped = False
         while stopped == False:
             WIN.fill(BLUE)
-            player.draw()
+            WIN.blit(player_image,(x,y))
             pygame.display.update()
             clock.tick(60)
             for event in pygame.event.get():
@@ -47,17 +34,13 @@ class MainRun:
                     pygame.quit()
                 if event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_UP:
-                        speedy -= 5
-                        speedx = 0
+                        v-=vel
                     if event.key == pygame.K_DOWN:
-                        speedy += 5
-                        speedx = 0
+                        y+=vel
                     if event.key == pygame.K_RIGHT:
-                        speedy = 0
-                        speedx += 0
+                        x+=vel
                     if event.key == pygame.K_LEFT:
-                        speedy = 0
-                        speedx -= 0
+                        x-=vel
                     if event.key == pygame.K_q:
                         stopped = True
                         pygame.quit()
